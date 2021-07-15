0d359bb31eca67e530435d747d48ca6ab6a06c76
Ema Čampa <85159765+emacampa3@users.noreply.github.com>
2021-07-15 11:28:01 +0200

    Add files via upload
    Recreating a right-aligned Mario pyramid, using hashes for bricks.

---
+++ mario.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+    int height;
+    do{
+        height = get_int("Height of pyramid: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++){
+        //spaces
+        for (int j = height - 1; j > i; j--){
+            printf(" ");
+        }
+
+        //hashes
+        for (int j = 0; j <= i; j++){
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
