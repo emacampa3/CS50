96df8524036659f9fdc09829b89ee3a6088afdbd
Ema Čampa <85159765+emacampa3@users.noreply.github.com>
2021-07-16 14:55:31 +0200

    Update population.c

--- Lab/population.c
+++ Lab/population.c
@@ -2,27 +2,32 @@
 #include <cs50.h>
 
 int main(void){
-    //Prompts a user for start_population
-    int start_population;
-    do{
-        start_population = get_int("Start size: ");
-    }
-    while (start_population < 9);
 
-    //Prompts a user for end_population
-    int end_population;
+    //Prompts the user for population at start
+    int start;
     do{
-        end_population = get_int("End size: ");
+        start = get_int("Start size: ");
     }
-    while (end_population < start_population);
+    while (start < 9);
+
+    //Prompts the user for population at the end
+    int end;
+    do{
+        end = get_int("End size: ");
+    }
+    while (end <= start);
 
     //counter for years passed
     int years = 0;
+
     //Updating the population until we reach end_population = start_population
-    while (end_population > start_population){
-        start_population = start_population + (start_population / 3) - (start_population / 4);
+    do{
+        start = start + (start / 3) - (start / 4);
         years++;
     }
+    while (end > start);
+
     //Printing the number of years needed
-    printf("The number is %i\n", years);
+    printf("Years: %i\n", years);
+
 }
